349cb64c536269ff1347d0a5b8e762ecc50386ca
Mohammad Nadeem <96985293+nadeemzafarnz@users.noreply.github.com>
2024-05-23 16:55:46 +0000

    conversion of data type

---
+++ datatupe2.js
@@ -0,0 +1,29 @@
+//conversion and confusion
+
+let score="nadeem"
+
+console.log(typeof score)
+/*
+let valueInNumber = Number (score)
+console.log(typeof score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+console.log(typeof null)
+console.log(typeof undefined)
+*/
+// "33" =>33
+// "33abc" => NaN
+// true => 1; false=o
+
+let isLoggedIn =""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+// 1 => true ; 0=>false
+//  "" => false
+// "nadeem" =>true
+
+let someNumber =33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
