726d3696a1c172e5d369d78bdbea4455708a8ed8
Mohammad Nadeem <96985293+nadeemzafarnz@users.noreply.github.com>
2024-05-23 16:39:57 +0000

    done today

---
+++ datatype.js
@@ -0,0 +1,18 @@
+"use strict";//treat all js code as newer version
+
+// alert("hello");we are using not js not a browers
+
+
+console.log(5+5)
+console.log("nadeem")
+
+let age=18
+let isloggedIn=true
+let state=null
+
+console.log(age, isloggedIn, state)
+console.log(typeof age)
+console.log(typeof isloggedIn)
+console.log(typeof state)
+console.log(typeof undefined)
+console.log(typeof true)
---
+++ variablein.js
@@ -0,0 +1,12 @@
+var a=10;
+var b=20;
+var c=a+b;
+console.log(c);
+
+const accountId=112233
+let accountEmail="nadeemzafarnz330907@gmail.com"
+var accountPassword="12345"
+accountCity="Najibabad"
+
+//accountId=33
+console.table([accountId, accountEmail, accountPassword, accountCity]);
