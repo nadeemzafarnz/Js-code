2e7a70f6601e5b1c460eb9b2cff81e57bc01706b
Mohammad Nadeem <96985293+nadeemzafarnz@users.noreply.github.com>
2024-05-23 18:01:19 +0000

    not completed

---
+++ Stringnumber.js
@@ -0,0 +1,32 @@
+let value =3
+
+let negValue= -value
+
+console.log(negValue)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2**2)
+console.log(2%2)
+
+let str1 ="hello"
+
+let str2 =" nadeem"
+
+let str3 = str1+str2
+
+console.log("1" +2)
+
+console.log(1 + "2")
+
+console.log("1" + 2 +2)
+
+console.log(1 + 2 + "2")
+
+console.log( (3 +4) * 5 % 3)
+
+console.log(+ true)
+
+
