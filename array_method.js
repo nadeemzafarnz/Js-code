fa7ed3eb02d310bd6e905c437b441d1b88f6b0a3
Mohammad Nadeem <96985293+nadeemzafarnz@users.noreply.github.com>
2024-06-09 20:18:47 +0000

    array metho part 1

---
+++ array_method.js
@@ -0,0 +1,24 @@
+//letral array method
+var cars=["audi","altu","nadem"]
+for(value of cars)
+    {
+     console.log(value);
+    }
+
+    //creating array using new instnce;
+    var Bike=new Array();
+     Bike[0]=["Honda Shine"];
+     Bike[1]=["Honda Hero"];
+     Bike[2]=["Discover"];
+    for (value of Bike)
+        {
+            console.log(value);
+                }
+
+            // Ceating array using Construtor.
+        
+            var Student=new Array("id","Name","Class","College Name");
+            for (value of Student)
+                {
+                    console.log(value);
+                }
