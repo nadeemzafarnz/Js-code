a5176a8d0b5d9e874bc0e76ae50da5c69ba19837
Mohammad Nadeem <96985293+nadeemzafarnz@users.noreply.github.com>
2024-06-09 19:26:56 +0000

    done

--- Stringnumber.js
+++ Stringnumber.js
@@ -1,9 +1,16 @@
+/*
+
 let value =3
 
 let negValue= -value
 
-console.log(negValue)
+console.log(negValue);
+console.log(value);
 
+let value1=8
+let posValue1=value1
+console.log(posValue1)
+//string compariztion
 console.log(2+2)
 console.log(2-2)
 console.log(2*2)
@@ -17,16 +24,35 @@ let str2 =" nadeem"
 
 let str3 = str1+str2
 
-console.log("1" +2)
+console.log(str3); 
 
-console.log(1 + "2")
+ */
 
-console.log("1" + 2 +2)
+console.log("1" + 2 );
 
-console.log(1 + 2 + "2")
+console.log(1 + "2");
 
-console.log( (3 +4) * 5 % 3)
+console.log("1" + 2 + 2);
 
-console.log(+ true)
+console.log(1 + 2 + "2");
 
+console.log( (3 + 4) * 5 % 3); 
 
+console.log( true);
+
+console.log(-"");
+
+let gameCounter=100;
+++gameCounter
+console.log(gameCounter)
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
---
+++ comparizion.js
@@ -0,0 +1,38 @@
+console.log(2>1);
+
+console.log(2>=1);
+
+console.log(1>2);
+console.log(2<1);
+console.log(2<=1);
+//console.log(2=2);
+console.log(2===2);
+console.log(2!=2);
+console.log(2!==2);
+//console.log(2!===2);
+
+console.log("nadeem zafar")
+console.log(null>0);
+console.log(null===0);
+console.log(null>=0);
+console.log(null==0);
+console.log("zafar ali");
+console.log(undefined==0);
+console.log(undefined);
+console.log("strckly check");
+console.log("2"===2);
+console.log(2===2);
+//with string
+
+console.log("2">1);
+
+console.log("02">=1);
+
+console.log(1>2);
+//console.log(2<1;
+console.log(2<=1);
+//console.log(2=2);
+console.log(2===2);
+console.log(2!=2);
+console.log(2!==2);
+//console.log(2!===2);
